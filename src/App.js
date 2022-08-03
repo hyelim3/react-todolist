5dd22684e19d24163316ebc72f836816d72205cf
hyelim3 <shl0703ss@gmail.com>
2022-08-04 00:45:04 +0900

    todolist 리뉴얼 todo 삭제

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ function App() {
   const [todos, setTodos] = useState([]);
   const onInsert = (text) => {
     //text를 받음
+
     const todo = {
       id: nextId.current,
       text: text, //text
       checked: false,
     };
+
     setTodos((todos) => todos.concat(todo)); //초기배열에 객체 넣어줌 -> 추가 반복
     nextId.current++;
   };
   const nextId = useRef(1); //추가할 때 id가 1부터 시작
 
+  const onRemove = (id) => {
+    // 해당 조건이 적용되는 것만 남긴다 todo의 id값이 다르면 남아있는다.
+    setTodos((todos) => todos.filter((todo) => todo.id !== id)); // 같으면 삭제
+  };
+
   const onToggle = (id) => {
     //아이디가 일치하는 id만 체크상태 바꾸기
     setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      todos.map(
+        (
+          todo //todo에서 todo안에 있는 id와 찾는id가 일치하면 바꿔야함
+        ) =>
+          //text, id 그대로 뿌려주고 checked만 바꿈 todo의 원래 상태의 반전
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo //나머지는 todo
       )
     );
   };
@@ -29,7 +40,7 @@ function App() {
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onToggle={onToggle} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos, onToggle }) => {
+const TodoList = ({ todos, onToggle, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo, index) => (
-        <TodoListItem todo={todo} key={index} onToggle={onToggle} />
+        <TodoListItem
+          todo={todo}
+          key={index}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
       ))}
     </div>
   );
